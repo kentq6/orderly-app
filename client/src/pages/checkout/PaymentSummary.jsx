7f983cdce306eaa690225336991881aa7c19a6e2
kentq6 <kentq6@gmail.com>
2025-09-03 17:06:25 -0400

    Created integration test for PaymentSummary component

--- client/src/pages/checkout/PaymentSummary.jsx
+++ client/src/pages/checkout/PaymentSummary.jsx
@@ -20,42 +20,48 @@ export function PaymentSummary({ paymentSummary, loadCart }) {
         <>
           <div className="payment-summary-row">
             <div>Items ({paymentSummary.totalItems}):</div>
-            <div className="payment-summary-money">
-              {formatMoney(paymentSummary.productCostCents)}
+            <div className="payment-summary-money"
+              data-testid="payment-summary-product-cost">
+                {formatMoney(paymentSummary.productCostCents)}
             </div>
           </div>
 
           <div className="payment-summary-row">
             <div>Shipping &amp; handling:</div>
-            <div className="payment-summary-money">
-              {formatMoney(paymentSummary.shippingCostCents)}
+            <div className="payment-summary-money"
+              data-testid="payment-summary-shipping-cost">
+                {formatMoney(paymentSummary.shippingCostCents)}
             </div>
           </div>
 
           <div className="payment-summary-row subtotal-row">
             <div>Total before tax:</div>
-            <div className="payment-summary-money">
-              {formatMoney(paymentSummary.totalCostBeforeTaxCents)}
+            <div className="payment-summary-money"
+              data-testid="payment-summary-total-cost-before-tax">
+                {formatMoney(paymentSummary.totalCostBeforeTaxCents)}
             </div>
           </div>
 
           <div className="payment-summary-row">
             <div>Estimated tax (10%):</div>
-            <div className="payment-summary-money">
-              {formatMoney(paymentSummary.taxCents)}
+            <div className="payment-summary-money"
+              data-testid="payment-summary-tax">
+                {formatMoney(paymentSummary.taxCents)}
             </div>
           </div>
 
           <div className="payment-summary-row total-row">
             <div>Order total:</div>
-            <div className="payment-summary-money">
-              {formatMoney(paymentSummary.totalCostCents)}
+            <div className="payment-summary-money"
+              data-testid="payment-summary-total-cost">
+                {formatMoney(paymentSummary.totalCostCents)}
             </div>
           </div>
 
           <button 
             className="place-order-button button-primary"
-            onClick={createOrder}>
+            onClick={createOrder}
+            data-testid="place-order-button">
               Place your order
           </button>
         </>
---
+++ client/src/pages/checkout/PaymentSummary.test.jsx
@@ -0,0 +1,82 @@
+import { it, expect, describe, vi, beforeEach } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter, useLocation } from 'react-router';
+import { PaymentSummary } from './PaymentSummary';
+import axios from 'axios';
+import userEvent from '@testing-library/user-event';
+
+vi.mock('axios');
+
+describe('PaymentSummary component', () => {
+  let paymentSummary;
+  let loadCart;
+  let user;
+
+  beforeEach(() => {
+    paymentSummary =
+    {
+      "totalItems": 7,
+      "productCostCents": 7630,
+      "shippingCostCents": 0,
+      "totalCostBeforeTaxCents": 7630,
+      "taxCents": 763,
+      "totalCostCents": 8393
+    }
+
+    loadCart = vi.fn();
+    user = userEvent.setup();
+  });
+
+  it('displays the correct details', () => {
+    render(
+      <MemoryRouter>
+        <PaymentSummary paymentSummary={paymentSummary} loadCart={loadCart} />
+      </MemoryRouter>
+    );
+
+    expect(
+      screen.getByText('Items (7):')
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByTestId('payment-summary-product-cost')
+    ).toHaveTextContent('$76.30');
+
+    expect(
+      screen.getByTestId('payment-summary-shipping-cost')
+    ).toHaveTextContent('$0.00');
+
+    expect(
+      screen.getByTestId('payment-summary-total-cost-before-tax')
+    ).toHaveTextContent('$76.30');
+
+    expect(
+      screen.getByTestId('payment-summary-tax')
+    ).toHaveTextContent('$7.63');
+
+    expect(
+      screen.getByTestId('payment-summary-total-cost')
+    ).toHaveTextContent('$83.93');
+  });
+
+  it('places an order', async () => {
+    function Location() {
+      const location = useLocation();
+      return <div data-testid="url-path">{location.pathname}</div>;
+    }
+
+    render(
+      <MemoryRouter>
+        <PaymentSummary paymentSummary={paymentSummary} loadCart={loadCart} />
+        <Location />
+      </MemoryRouter>
+    );
+
+    const placeOrderButton = screen.getByTestId('place-order-button');
+    await user.click(placeOrderButton);
+
+    expect(axios.post).toHaveBeenCalledWith('/api/orders');
+    expect(loadCart).toHaveBeenCalled();
+    expect(screen.getByTestId('url-path')).toHaveTextContent('/orders');
+  });
+});
