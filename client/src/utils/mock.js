05275e8db4134932978bc296b8528dd6316d3eef
kentq6 <kentq6@gmail.com>
2025-09-05 19:13:58 -0400

    Changed mock file name

---
+++ client/src/utils/mock.js
@@ -0,0 +1,51 @@
+export const cart = [
+  {
+    "id": 9,
+    "productId": "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    "quantity": 2,
+    "deliveryOptionId": "1",
+    "createdAt": "2025-08-28T02:34:04.409Z",
+    "updatedAt": "2025-09-05T02:55:55.433Z",
+    "product": {
+      "keywords": [
+        "socks",
+        "sports",
+        "apparel"
+      ],
+      "id": "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      "image": "images/products/athletic-cotton-socks-6-pairs.jpg",
+      "name": "Black and Gray Athletic Cotton Socks - 6 Pairs",
+      "rating": {
+        "stars": 4.5,
+        "count": 87
+      },
+      "priceCents": 1090,
+      "createdAt": "2025-08-26T17:36:02.611Z",
+      "updatedAt": "2025-08-26T17:36:02.611Z"
+    }
+  },
+  {
+    "id": 10,
+    "productId": "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    "quantity": 1,
+    "deliveryOptionId": "1",
+    "createdAt": "2025-09-05T02:56:28.446Z",
+    "updatedAt": "2025-09-05T02:56:28.446Z",
+    "product": {
+      "keywords": [
+        "sports",
+        "basketballs"
+      ],
+      "id": "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      "image": "images/products/intermediate-composite-basketball.jpg",
+      "name": "Intermediate Size Basketball",
+      "rating": {
+        "stars": 4,
+        "count": 127
+      },
+      "priceCents": 2095,
+      "createdAt": "2025-08-26T17:36:02.612Z",
+      "updatedAt": "2025-08-26T17:36:02.612Z"
+    }
+  }
+];
