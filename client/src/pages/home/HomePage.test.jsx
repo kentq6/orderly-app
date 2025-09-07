466ec7530bb48a6f009539771595567e6f0320ab
kentq6 <kentq6@gmail.com>
2025-09-07 18:34:18 -0400

    Used mockProducts from mock.js

--- client/src/pages/home/HomePage.test.jsx
+++ client/src/pages/home/HomePage.test.jsx
@@ -2,6 +2,7 @@ import { it, expect, describe, vi, beforeEach } from 'vitest';
 import { render, screen, within } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
 import { HomePage } from './HomePage';
+import { products as mockProducts } from '../../utils/mock'
 import axios from 'axios';
 import userEvent from '@testing-library/user-event';
 
@@ -9,38 +10,17 @@ vi.mock('axios');
 
 describe('HomePage component', () => {
   let loadCart;
+  let products;
   let productContainers;
   let user;
 
   beforeEach(async () => {
+    products = mockProducts;
     loadCart = vi.fn();
 
     axios.get.mockImplementation(async (urlPath) => {
       if (urlPath === '/api/products') {
-        return {
-          data: [{
-            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-            rating: {
-              stars: 4.5,
-              count: 87
-            },
-            priceCents: 1090,
-            keywords: ["socks", "sports", "apparel"]
-          },
-          {
-            id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-            image: "images/products/intermediate-composite-basketball.jpg",
-            name: "Intermediate Size Basketball",
-            rating: {
-              stars: 4,
-              count: 127
-            },
-            priceCents: 2095,
-            keywords: ["sports", "basketballs"]
-          }]
-        };
+        return { data: products };
       }
     });
 
