b62fbb4d984289075caf97580976c5c8f1322881
kentq6 <kentq6@gmail.com>
2025-09-18 17:19:18 -0400

    Define cart type for reusability

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,13 +5,10 @@ import SearchIcon from '../assets/images/icons/search-icon.png';
 import LogoWhite from '../assets/images/logo-white.png';
 import MobileLogoWhite from '../assets/images/mobile-logo-white.png';
 import './Header.css';
+import type { Cart } from '../types/cart.types';
 
 type HeaderProps = {
-  cart: {
-    productId: string;
-    quantity: number;
-    deliveryOptionId: string;
-  }[];
+  cart: Cart[];
 };
 
 export function Header({ cart }: HeaderProps) {
