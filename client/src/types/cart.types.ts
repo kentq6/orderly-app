66dc929d1da15b81a0717e52d85530c94296780c
kentq6 <kentq6@gmail.com>
2025-09-19 14:56:11 -0400

    Redefine cart type

--- client/src/types/cart.types.ts
+++ client/src/types/cart.types.ts
@@ -1,5 +1,21 @@
 export type Cart = {
+  createdAt: string;
+  deliveryOptionId: string;
+  id: number;
+  product: {
+    createdAt: string;
+    id: string;
+    image: string;
+    keywords: string[];
+    name: string;
+    priceCents: number;
+    rating: {
+      count: number;
+      stars: number;
+    };
+    updatedAt: string;
+  };
   productId: string;
   quantity: number;
-  deliveryOptionId: string;
+  updatedAt: string;
 }[];
